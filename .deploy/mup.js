f4fe9711df376409cfda926ae45ed70ed8cd9c60
Zachary Dixon <rissk13@gmail.com>
2016-08-24 12:40:22 -0500

    Updated mup

---
+++ .deploy/mup.js
@@ -0,0 +1,28 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '45.55.204.214',
+      username: 'root'
+      // pem:
+      // password:
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'app',
+    path: '../app',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'www.highwirecomputer.com',
+    },
+
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime: 60
+  }
+};
